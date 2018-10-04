66c67750a167a6396961aa02ca13a36ca60c4f36
Dang (Wilber) Nguyen(minhdang.net@gmail.com) <minhdang.net@gmail.com>
2018-10-04 15:23:47 +0700

    fix publish
    [no card]

--- scripts/publish.rb
+++ scripts/publish.rb
@@ -3,4 +3,4 @@ require 'json'
 manifest_path = File.expand_path('../../app.json', __FILE__)
 version = JSON.parse(File.read(manifest_path))['version']
 
-exec("gem build google_json_response.gemspec && curl -F package=@google_json_response_#{version}.gem https://#{ENV['GEMFURY_TOKEN']}@push.fury.io/#{ENV['GEMFURY_PACKAGE']}/")
+exec("gem build google_json_response.gemspec && curl -F package=@google_json_response-#{version}.gem https://#{ENV['GEMFURY_TOKEN']}@push.fury.io/#{ENV['GEMFURY_PACKAGE']}/")
